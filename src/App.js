c61c81f2ec873d779e1a1ab55e8f746357be911d
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-10-17 16:21:15 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import BankDetails from './components/BankDetails';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header className="" />
+      <div className='flex flex-row'>
+        <div className='hidden lg:block'>
+          <Sidebar />
+        </div>
+        <div className=''>
+          <BankDetails />
+        </div>
+      </div>
     </div>
   );
 }
